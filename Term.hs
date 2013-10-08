22dcbfa75473e2a8fde23f434f2b606f15000051
Eloi Perdereau <eloi@perdereau.eu>
2013-10-08 17:56:57 +0200

    add Term.hs with some types and functions definitions

---
+++ Term.hs
@@ -0,0 +1,58 @@
+-- @File Term.hs
+-- @Author Sebastien DELECRAZ, Eloi PERDEREAU
+-- @Date 08-10-2013
+
+module Term (
+    Var,
+    FuncSymb,
+    Signature,
+    Term,
+    varToTerm,
+    constructTerm,
+    variables,
+    sig,
+    Substitution,
+    identity,
+    (*!),
+    -- (@@)
+) where
+
+type Var = String
+
+type FuncSymb = String
+
+type Signature = [(FuncSymb, Int)]
+
+data Term v = TermVar v | TermFunc FuncSymb [Term v]
+
+varToTerm :: Var -> Term Var
+varToTerm v = TermVar v
+
+constructTerm :: FuncSymb -> [Term v] -> Term v
+constructTerm f ts = TermFunc f ts
+
+variables :: Term v -> [v]
+variables (TermVar v) = [v]
+variables (TermFunc f ts) = concat (map variables ts)
+
+sig :: Term v -> Signature
+sig (TermVar _) = []
+sig (TermFunc f ts) = (f, length ts):concat (map sig ts)
+
+type Substitution v = [(v, Term v)]
+
+identity :: Substitution a
+identity = []
+
+(*!) :: Eq v => Term v -> Substitution v -> Term v
+(*!) t [] = t
+(*!) (TermVar v) (s:ss) = if fst s == v then snd s
+                                        else (TermVar v) *! ss
+(*!) (TermFunc f ts) s = TermFunc f (map (\x -> x *! s) ts)
+
+-- (@@)
+
+-- examples
+
+terme0 = TermVar "x"
+terme1 = TermFunc "f" [TermFunc "g" [TermVar "x"], TermVar "y"]
