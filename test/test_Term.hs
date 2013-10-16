40802fe3d577ba0caff7567666df3935c2ffb45e
Eloi Perdereau <eloi@perdereau.eu>
2013-10-16 22:55:55 +0200

    add test functions (printArbitrary will be removed)

--- test/test_Term.hs
+++ test/test_Term.hs
@@ -5,6 +5,7 @@
 import Term
 import Test.QuickCheck.Gen
 import Test.QuickCheck.Arbitrary
+import Data.Maybe
 
 instance (Arbitrary v, Arbitrary f) => Arbitrary (Term v f) where
     arbitrary = arbitraryTerm
@@ -47,3 +48,30 @@ instance (Arbitrary v, Arbitrary f, Eq v) => Arbitrary (TSubstitution v f) where
 --      ie remove one of theses [(x, TermVar y), (x, TermVar z)]
 cleanSubstitution :: Eq v => Substitution v f -> Substitution v f
 cleanSubstitution ss = filter (not.(uncurry compareVar)) ss
+
+-- printArbitrary :: Gen (Term Char Char) -> IO()
+-- printArbitrary :: Gen (TSubstitution Char Char) -> IO()
+-- printArbitrary a = do
+--                     s <- newStdGen
+--                     print $ unGen a s 100
+
+testApplicationIdentity :: (Eq v, Eq f) => Term v f -> Bool
+testApplicationIdentity t = t *! identity == t
+
+testCompositionIdentity
+  :: (Eq v, Eq f) => TSubstitution v f -> Bool
+testCompositionIdentity (TSubstitution s) = s @@ identity == s &&
+                                            s == identity @@ s
+
+testCompositionAssociativeness
+  :: (Eq v, Eq f) =>
+     TSubstitution v f -> TSubstitution v f -> TSubstitution v f -> Bool
+testCompositionAssociativeness (TSubstitution s)
+                               (TSubstitution t)
+                               (TSubstitution u) = (s @@ t) @@ u == s @@ (t @@ u)
+
+testUnif :: UnifProblem Char Char -> Bool
+testUnif ps = let maybeU = unif ps
+              in if isNothing maybeU then False
+                 else let u = fromJust maybeU
+                      in null $ filter (\c -> fst c *! u /= snd c *! u) ps
