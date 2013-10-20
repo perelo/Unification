f840483e1b790f2a8eb3691d5a666a6a0f6b0836
Eloi Perdereau <eloi@perdereau.eu>
2013-10-20 12:07:31 +0200

    move arbitrary definitions in ArbitraryTerm.hs

--- test/arbitraryTerm.hs
+++ test/arbitraryTerm.hs
@@ -2,6 +2,10 @@
 -- @Author Eloi PERDEREAU
 -- @Date 10-10-2013
 
+module ArbitraryTerm (
+  TSubstitution(..)
+) where
+
 import Term
 import Test.QuickCheck.Gen
 import Test.QuickCheck.Arbitrary
@@ -55,31 +59,4 @@ cleanSubstitution ss = let s0 = filter (not.(uncurry compareVar)) ss
 -- printArbitrary a = do
 --                     s <- newStdGen
 --                     print $ unGen a s 100
-
-testApplicationId :: (Eq v, Eq f) => Term v f -> Bool
-testApplicationId t = t *! identity == t
-
-testComposId
-  :: (Eq v, Eq f) => TSubstitution v f -> Bool
-testComposId (TSubstitution s) = s @@ identity == s &&
-                                 s == identity @@ s
-
-testComposAssoc
-  :: (Ord v, Ord f) =>
-     TSubstitution v f -> TSubstitution v f -> TSubstitution v f -> Bool
-testComposAssoc (TSubstitution rho) (TSubstitution sigma) (TSubstitution tau) =
-                (rho @@  sigma) @@ tau == rho @@ (sigma  @@ tau)
-
-testComposDef
-  :: (Eq v, Eq f) =>
-     (Term v f) -> (TSubstitution v f) -> (TSubstitution v f) -> Bool
-testComposDef t (TSubstitution sigma) (TSubstitution tau) =
-                (t *! sigma) *! tau == t *! (tau @@ sigma)
-
-testUnif :: (Eq a, Eq b) => UnifProblem a b -> Bool
-testUnif ps = isNothing maybeU ||
-              null (filter (\(var,term) -> var *! u /= term *! u) ps)
-                where
-                  maybeU = unif ps
-                  u = fromJust maybeU
 
---
+++ test/testTerm.hs
@@ -0,0 +1,35 @@
+-- @File test_Term.hs
+-- @Author Eloi PERDEREAU
+-- @Date 10-10-2013
+
+import Term
+import ArbitraryTerm
+import Data.Maybe
+
+testApplicationId :: (Eq v, Eq f) => Term v f -> Bool
+testApplicationId t = t *! identity == t
+
+testComposId
+  :: (Eq v, Eq f) => TSubstitution v f -> Bool
+testComposId (TSubstitution s) = s @@ identity == s &&
+                                 s == identity @@ s
+
+testComposAssoc
+  :: (Ord v, Ord f) =>
+     TSubstitution v f -> TSubstitution v f -> TSubstitution v f -> Bool
+testComposAssoc (TSubstitution rho) (TSubstitution sigma) (TSubstitution tau) =
+                (rho @@  sigma) @@ tau == rho @@ (sigma  @@ tau)
+
+testComposDef
+  :: (Eq v, Eq f) =>
+     (Term v f) -> (TSubstitution v f) -> (TSubstitution v f) -> Bool
+testComposDef t (TSubstitution sigma) (TSubstitution tau) =
+                (t *! sigma) *! tau == t *! (tau @@ sigma)
+
+testUnif :: (Eq a, Eq b) => UnifProblem a b -> Bool
+testUnif ps = isNothing maybeU ||
+              null (filter (\(var,term) -> var *! u /= term *! u) ps)
+                where
+                  maybeU = unif ps
+                  u = fromJust maybeU
+
